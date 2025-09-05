84a513aae06a0329bc3c0300bf7f770c224aa205
MAX2YT <2kmaxkarthi@gmail.com>
2025-09-05 23:19:54 +0530

    Landing page & scroll fix (5/9/25) 11.19pm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
@@ -8,21 +8,26 @@ import Services from './pages/Services/Services';
 import Contact from './pages/Contact/Contact';
 import Careers from './pages/Careers/Careers';
 import Team from './pages/Team/Team';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/eks-constructions">
       <div className="App">
+        <ScrollToTop />
         <Header />
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/careers" element={<Careers />} />
             <Route path="/team" element={<Team />} />
+            {/* Catch all route - redirect to home */}
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
         <Footer />
